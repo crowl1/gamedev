b686c8681ea8eae0dba5df499a4939cf06200770
icrowli <i.andriy.dovgalyuk@gmail.com>
2022-01-11 23:57:15 +0200

    малювання гравців

--- main.py
+++ main.py
@@ -21,9 +21,21 @@ while run:
     
     x, y = 0, 0
 
+    green_payer_point = (3,1)
+    green_payer_coordinate = ((green_payer_point[0]-1)*2*height, (green_payer_point[1]-1)*2*width)
+
+    red_payer_point = (4,6)
+    red_payer_coordinate = ((red_payer_point[0]-1)*2*height, (red_payer_point[1]-1)*2*width)
+
     while x < x_field:
         while y < y_field:
-            pygame.draw.rect(win, (0,0,255), (x, y, width, height))
+
+            if (x, y) == red_payer_coordinate:
+                pygame.draw.rect(win, (255,0,0), (x, y, width, height))
+            elif (x, y) == green_payer_coordinate:
+                pygame.draw.rect(win, (0,255,0), (x, y, width, height))
+            else:
+                pygame.draw.rect(win, (0,0,255), (x, y, width, height))
 
             y += 100
         
